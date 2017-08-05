c33d8f4ea170bd2a5055b885322e474cc6a69fbd
yunsonbai <1942893504@qq.com>
2017-08-06 00:23:13 +0800

    update words_2_img

--- eagleocr/split/wordsToImg.py
+++ eagleocr/split/wordsToImg.py
@@ -30,9 +30,13 @@ def words_2_img(img_2v, sf, save_folder, trans=True, hyaline=True):
     if trans is True:
         img_array = img_array.transpose()
     position = _word_position(img_array, sf, transpose=trans)
+    wnames = {}
     for k in position.keys():
         x = position[k][0]
         y = position[k][1]
         word_array = _get_word(x, y, img_array, trans)
-        img_name = '{0}/{1}.png'.format(save_folder, k)
+        name = '{0}.png'.format(k)
+        wnames[k] = name
+        img_name = '{0}/{1}'.format(save_folder, name)
         _save_word_img(word_array, img_name, hyaline=True)
+    return wnames
