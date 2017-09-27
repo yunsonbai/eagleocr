6903f5dd96cc8e49ee7537bbb0e569295421c773
白松 <yunsonbai@sohu-inc.com>
2017-09-27 11:23:47 +0800

    update

--- eagleocr/lib/imgDenoise.py
+++ eagleocr/lib/imgDenoise.py
@@ -9,6 +9,10 @@ def denoisecolor(img):
     denoise color img
     '''
     img_array = numpy.array(img)
+    try:
+        img_array = cv2.cvtColor(img_array, cv2.COLOR_GRAY2BGR)
+    except Exception as e:
+        pass
     dst = cv2.fastNlMeansDenoisingColored(
         img_array, None, 10, 10, 7, 21)
     return Image.fromarray(dst)
