ed0c56b88d3b54652e20dd18b91e6556a2ec22a3
白松 <yunsonbai@sohu-inc.com>
2017-09-27 16:55:00 +0800

    update

---
+++ REAME.md
@@ -0,0 +1,30 @@
+# eagleocr
+## Overview
+* 应用：识别图片中的文字
+* 版本：0.1
+* 安装：
+    * git clone git@github.com:yunsonbai/eagleocr.git
+    * python setup.py install
+* 已经实现的功能:
+    * 文字切割
+
+* 还需完成的功能：
+    * 文字学习
+    * 文字识别
+    * 图片矫正
+
+## Requirements
+* 开发语言
+    * python3.5
+* 依赖库
+    * PIL==4.1.1
+    * opencv-python==3.2.0.7
+    * numpy==1.13.0
+
+
+## Quick Start
+ [Test Script](https://github.)
+
+#### note
+
+## Documentation
---
+++ setup.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import os
+from setuptools import setup
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+setup(
+    name="eagleocr",
+    version="0.1.2",
+    packages=get_packages('eagleocr'),
+    author="yunsonbai",
+    author_email='1942893504@qq.com',
+    url="http://www.yunsonbai.top",
+    description='Image text recognition',
+    install_requires=[
+        'Pillow==3.2.0', 'opencv-python==3.2.0.7', 'numpy==1.13.0',
+        'tesserocr==2.2.2'],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
